2f6e62805d45fae66e8784bdc3383bee23520d9b
Syed Ammad <77102473+AmmadKhanM@users.noreply.github.com>
2024-08-17 00:34:59 +0200

    Logout functionality added

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,10 @@
 
 export default function UserProfile({params}: any) {
   return (
-    <div>Profile page {params.id}</div>
+    <>
+    <div className="max-w-md mx-auto p-6 border border-gray-300 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">User profile page {params.id}</h2>
+    </div>
+    </>
   )
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,42 @@
+"use client"
+
+import axios from "axios";
+import { useRouter} from "next/navigation";
+import { useState } from "react";
+
 
 export default function Profile() {
+  const router = useRouter();
+  const [loading, setloading] = useState(false);
+
+  const logout = async () => {
+    try {
+      setloading(true);
+      const reqBody = await axios.get('/api/users/logout')
+      console.log(reqBody);
+      router.push('/login')
+            
+    } catch (error: any) {
+      console.log('Logout failed', error.message)
+    }finally{
+      setloading(false)
+    }
+  }
   return (
-    <div>Profile page</div>
+
+    <>
+    <div className="max-w-md mx-auto p-6 border border-gray-300 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">{loading ? 'processing..' : 'Profile Page'}</h2>
+        <button
+          onClick={logout}
+          type="submit"
+          className="w-full py-2 px-4 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        >
+          logout
+        </button>
+    </div>
+    </>
+
+    
   )
 }
